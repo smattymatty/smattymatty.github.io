c446578b0db93df4b9cdd3a9fa5671d0b5808ece
smattymatty <smattymatt@gmail.com>
2024-10-19 11:17:19 -0400

    nav"

--- main.mjs
+++ main.mjs
@@ -1,1 +1,19 @@
 console.log("Hello World");
+
+const navLinks = document.querySelectorAll(".nav-links button");
+const contentDivs = document.querySelectorAll(".content");
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const content = document.querySelector(`#${link.id}-content`);
+    contentDivs.forEach((content) => {
+      content.classList.add("hidden");
+    });
+    content.classList.remove("hidden");
+    navLinks.forEach((navLink) => {
+      navLink.classList.remove("active");
+    });
+    link.classList.add("active");
+  });
+});
