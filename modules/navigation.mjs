746e9eccdb191d95177bec585c85b0362243cc1a
smattymatty <smattymatt@gmail.com>
2024-10-20 01:48:47 -0400

    cooler buttons and the sidebar

--- modules/navigation.mjs
+++ modules/navigation.mjs
@@ -3,20 +3,30 @@ export function setupNavLinks(initialActive = "home") {
   const contentDivs = document.querySelectorAll(".content");
   // each nav link should have a corresponding content div
   navLinks.forEach((link) => {
-    link.addEventListener("click", (e) => {
+    link.addEventListener("mousedown", (e) => {
       e.preventDefault();
+      // click animation
+      link.classList.add("click-animation");
       const content = document.querySelector(`#${link.id}-content`);
       if (!content) {
         throw new Error(`No content with id ${link.id}-content`);
       }
+      // hide all content divs
       contentDivs.forEach((content) => {
         content.classList.add("hidden");
       });
+      // show the content div for the clicked nav link
       content.classList.remove("hidden");
+      // remove active class from all nav links
       navLinks.forEach((navLink) => {
         navLink.classList.remove("active");
       });
+      // add active class to the clicked nav link
       link.classList.add("active");
+      // remove click animation
+      setTimeout(() => {
+        link.classList.remove("click-animation");
+      }, 370);
     });
   });
   // set up the initial active nav link
---
+++ sidebarContent.mjs
@@ -0,0 +1,69 @@
+/**
+ * Sidebar Setup
+ *
+ * A JS object that cointains the sidebar's setup for each page, and it's corresponding content.
+ *
+ */
+export const sidebarSetup = {
+  home: {
+    welcomeMessage: "Welcome to my website",
+    1: {
+      title: "home 1",
+      content: "home 1 content",
+    },
+    2: {
+      title: "home 2",
+      content: "home 2 content",
+    },
+    3: {
+      title: "home 3",
+      content: "home 3 content",
+    },
+  },
+
+  projects: {
+    welcomeMessage: "Welcome to my projects",
+    1: {
+      title: "projects 1",
+      content: "projects 1 content",
+    },
+    2: {
+      title: "projects 2",
+      content: "projects 2 content",
+    },
+    3: {
+      title: "projects 3",
+      content: "projects 3 content",
+    },
+  },
+  about: {
+    welcomeMessage: "Welcome to my about",
+    1: {
+      title: "about 1",
+      content: "about 1 content",
+    },
+    2: {
+      title: "about 2",
+      content: "about 2 content",
+    },
+    3: {
+      title: "about 3",
+      content: "about 3 content",
+    },
+  },
+  contact: {
+    welcomeMessage: "Welcome to my contact",
+    1: {
+      title: "contact 1",
+      content: "contact 1 content",
+    },
+    2: {
+      title: "contact 2",
+      content: "contact 2 content",
+    },
+    3: {
+      title: "contact 3",
+      content: "contact 3 content",
+    },
+  },
+};
